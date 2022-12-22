37abf18309fdb6b57178a1684b8f9ea74cc4159b
Yasser B. Lebon <lebon.yasser@skiff.com>
2022-12-22 12:12:33 +0100

    Init contacts

---
+++ vespa_contacts.py
@@ -0,0 +1,9 @@
+def list_contacts():
+    """List contacts"""
+    contacts = ["gaia", "akki", "eva", "thomas", "helene", "andrea", "..."]
+    for contact in contacts:
+        print(f"- {contact}")
+
+
+if __name__ == "__main__":
+    list_contacts()
