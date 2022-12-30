557c07e7a34ac0bcc1b58b1ed6f8b5be50c61b6e
Yasser B. Lebon <lebon.yasser@skiff.com>
2022-12-30 17:29:03 +0400

    Update vespa_contacts.py
    implement locate contact

--- vespa_contacts.py
+++ vespa_contacts.py
@@ -3,6 +3,7 @@
 Usage:
   vespa_contacts.py list
   vespa_contacts.py add <name>
+  vespa_contacts.py location <name>
   vespa_contacts.py (-h | --help)
 
 Examples:
@@ -14,19 +15,35 @@ Options:
 """
 from docopt import docopt
 
-CONTACTS = ["gaia", "akki", "eva", "thomas", "helene", "andrea"]
+CONTACTS = {
+  "gaia": {"loc": "the netherlands"},
+  "akki": {"loc": "mauritius"},
+  "eva": {"loc": "ireland"},
+  "thomas": {"loc": "france"},
+  "helene": {"loc": "france"},
+  "andrea": {"loc": "the hague"},
+  "sebastien": {"loc": "reunion"}
+}
 
 
 def add_contact(name):
     """Add contact to the vespa"""
-    CONTACTS.append(name)
+    CONTACTS[name] = dict()
     list_contacts()
-
+    
+def locate_contact(name):
+    """Return contact location"""
+    contact_info = CONTACTS.get(name, {})
+    if not contact_info:
+      return "contact not found"
+    else:
+      contact_loc = contact_info.get('loc', 'location not provided')
+      return contact_loc
 
 def list_contacts():
     """List contacts"""
-    for contact in CONTACTS:
-        print(f"- {contact}")
+    for name in CONTACTS.keys():
+        print(f"- {name}")
 
 
 if __name__ == "__main__":
@@ -35,3 +52,6 @@ if __name__ == "__main__":
         list_contacts()
     elif arguments.get('add'):
         add_contact(arguments['<name>'])
+    elif arguments.get('locate'):
+        print(locate_contact(arguments['<name>']))
+        
